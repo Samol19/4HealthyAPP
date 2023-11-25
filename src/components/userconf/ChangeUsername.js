06450ab4379dbbd84c5b0c24c3802729906c7543
Samuel Paredes <135689277+Samol19@users.noreply.github.com>
2023-11-25 02:12:38 -0500

    Update ChangeUsername.js

--- src/components/userconf/ChangeUsername.js
+++ src/components/userconf/ChangeUsername.js
@@ -38,7 +38,7 @@ export const ChangeUsername = () => {
         event.preventDefault();
   //PROBAMOS CON EMAIL TEST
         if (canIn) {
-            axios.put('http://localhost:3000/updateuser', { id:"1", username:password })
+            axios.put('https://api-library-v1.onrender.com/updateuser', { id:"1", username:password })
             
             .then(response => {
               alert("La Solicitud de cambio fue exitosa, puede volver al inicio");
